d4623cf3e6f08ad6ef65cde48234d7086e9e2c34
kyoushiro3 <starkjarvis99@gmail.com>
2024-10-13 20:45:05 +0800

    fixed: import label

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
-import { Label } from "@radix-ui/react-label";
+import { Label } from "@/components/ui/label";
 import React, { useState } from "react";
 
 interface InputFormProps {
